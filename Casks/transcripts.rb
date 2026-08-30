5e597142eb3a94404d15e256b3237a9c27cda148
Doug Hatcher <superterran@gmail.com>
2026-08-29 21:08:27 -0400

    transcripts 1.0.6

--- Casks/transcripts.rb
+++ Casks/transcripts.rb
@@ -1,6 +1,6 @@
 cask "transcripts" do
-  version "1.0.5"
-  sha256 "83fb5c2a780af7646fcc7fff0bf2d7eba7020904b4f002f92c54e82f91714d62"
+  version "1.0.6"
+  sha256 "9d187677d2c0830657f74255340044265af683f8576befead5cdcb246f93e936"
 
   url "https://transcripts.hatcher.ltd/Transcripts-#{version}.zip"
   name "Transcripts"
